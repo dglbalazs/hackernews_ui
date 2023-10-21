6655f6b4c74ec50ff68a044b286ed7189099e36a
diagolbalazs <diagolbalazs@gmail.com>
2023-10-21 20:36:45 +0200

    Init project with Vite + added Base URL for API

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quas aliquid
+    nam iste atque quo minus non eveniet molestias, ut ab, dolorem vitae
+    possimus placeat! Suscipit et consequuntur nostrum vitae.
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <TheWelcome />
+    {{ url }}
+  </main>
+</template>
+
+<script lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+
+export default {
+  components: { TheWelcome },
+  props: ["url"],
+};
+</script>
