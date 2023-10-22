a04e6ff9154e6f6a3e382dc36f9ee16f923a293a
diagolbalazs <diagolbalazs@gmail.com>
2023-10-22 13:22:46 +0200

    Added Type declarations to functions

--- src/components/functions.ts
+++ src/components/functions.ts
@@ -1,7 +1,7 @@
+import type { Item } from "@/components/types"
 
-
-export async function getData(baseUrl: string) {
-    const response = await fetch(baseUrl + "/topstories.json")
+export async function getData(baseUrl: string): Promise<string[]> {
+    const response = await fetch(baseUrl + '/topstories.json?limitToFirst=10&orderBy="$key"')
     if (!response.ok) {
         throw new Error("Failed to fetch top stories")
     }
@@ -9,7 +9,7 @@ export async function getData(baseUrl: string) {
 }
 
 
-export async function getItem(baseUrl: string, itemId: string) {
+export async function getItem(baseUrl: string, itemId: string): Promise<Item> {
     const response = await fetch(baseUrl + "/item/" + itemId + ".json")
     if (!response.ok) {
         throw new Error("Failed to fetch item")
@@ -17,20 +17,22 @@ export async function getItem(baseUrl: string, itemId: string) {
     return await response.json()
 }
 
-export async function getCommentCount(baseUrl: string, itemId: string) {
+// Not needed function, was just trying out looping items
+// - however, it seems like the default descendant value which should remark the total comment count on an item is not always in synch with the actual comment number
+// - sometimes descedants is lagging, and is not refreshed as quickly as the actual total comment count, however, using that is still a quicker solution that looping through all comments
+
+export async function getCommentCount(baseUrl: string, itemId: string): Promise<number> {
     const response = await fetch(baseUrl + "/item/" + itemId + ".json")
     if (!response.ok) {
         throw new Error("Failed to fetch item")
     }
 
     // console.log(itemId)
-    const data = await response.json()
-    console.log(data)
-    if ("deleted" in data && data.deleted) {
-        console.warn(data)
+    const data = await response.json() as Item;
+    if (data.deleted) {
         return -1
     }
-    if (!("kids" in data)) return 0
+    if (!data.kids) return 0
     const kids = data.kids
 
     if (!(kids.length > 0) ) return 0
@@ -44,4 +46,9 @@ export async function getCommentCount(baseUrl: string, itemId: string) {
     // Sum up the counts
     return counts.reduce((acc, curr) => acc + curr, 0);
 }
+
+// CONSIDERATIONS
+
+// - Stories do have count and all informations
+// - Data fetch timeout and it false, then show a refresh error page
 
